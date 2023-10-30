bb77b4189bca162fb1ececf01cd27b6f32298e5d
HyunggiPark <phg9587@naver.com>
2023-10-30 22:26:43 +0900

    Naver 지역 검색 API TDD로 구현하기 - 실행 코드 구현

--- src/test/java/com/example/presenting_tdd/NaverSearchAPITest.java
+++ src/test/java/com/example/presenting_tdd/NaverSearchAPITest.java
@@ -1,7 +1,58 @@
 package com.example.presenting_tdd;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class WebClientConfig {
+    @Value("${naver.client.id}")
+    private String clientId;
+
+    @Value("${naver.client.secret}")
+    private String clientSecret;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://openapi.naver.com/v1/search/local.json")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Naver-Client-Id", clientId)
+                .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+    }
+}
 
 @SpringBootTest
-public class NaverSearchAPITest {
+class NaverSearchAPITest {
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    void testApiCall() {
+        String query = "주식";
+        int display = 5;
+        int start = 1;
+        String sort = "random";
+
+        String response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("query", query)
+                        .queryParam("display", display)
+                        .queryParam("start", start)
+                        .queryParam("sort", sort)
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(response);
+    }
 }
