bacf0822284e0f75287990a8710207faf4a37d3e
HyunggiPark <phg9587@naver.com>
2023-10-31 22:41:29 +0900

    위임자 추출 및 Configuration 옮긴 후 Autowired 설정

---
+++ src/test/java/com/example/presenting_tdd/NaverSearchAPI.java
@@ -0,0 +1,60 @@
+package com.example.presenting_tdd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class NaverSearchAPI {
+    @Autowired
+    WebClient webClient;
+
+    public NaverSearchAPI() {
+    }
+
+    SearchResult search() {
+        String query = "주식";
+        int display = 5;
+        int start = 1;
+        String sort = "random";
+
+        SearchResult response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("query", query)
+                        .queryParam("display", display)
+                        .queryParam("start", start)
+                        .queryParam("sort", sort)
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(SearchResult.class)
+                .block();
+
+        return response;
+    }
+
+    @Configuration
+    static
+    class WebClientConfig {
+        @Value("${naver.client.id}")
+        private String clientId;
+
+        @Value("${naver.client.secret}")
+        private String clientSecret;
+
+        @Bean
+        public WebClient webClient() {
+            return WebClient.builder()
+                    .baseUrl("https://openapi.naver.com/v1/search/local.json")
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .defaultHeader("X-Naver-Client-Id", clientId)
+                    .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                    .build();
+        }
+    }
+}
--- src/test/java/com/example/presenting_tdd/NaverSearchAPITest.java
+++ src/test/java/com/example/presenting_tdd/NaverSearchAPITest.java
@@ -2,67 +2,26 @@ package com.example.presenting_tdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Configuration
-class WebClientConfig {
-    @Value("${naver.client.id}")
-    private String clientId;
-
-    @Value("${naver.client.secret}")
-    private String clientSecret;
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl("https://openapi.naver.com/v1/search/local.json")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("X-Naver-Client-Id", clientId)
-                .defaultHeader("X-Naver-Client-Secret", clientSecret)
-                .build();
-    }
-}
-
 @SpringBootTest
 class NaverSearchAPITest {
     @Autowired
-    private WebClient webClient;
+    private NaverSearchAPI naverSearchAPI;
 
     @Test
     void testApiCall() {
-        final SearchResult search = search();
-        assertThat(search.lastBuildDate()).hasSize(5);
+        final SearchResult search = naverSearchAPI.search();
+        assertThat(search.items()).hasSize(5);
     }
 
     private SearchResult search() {
-        String query = "주식";
-        int display = 5;
-        int start = 1;
-        String sort = "random";
 
-        SearchResult response = webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("query", query)
-                        .queryParam("display", display)
-                        .queryParam("start", start)
-                        .queryParam("sort", sort)
-                        .build())
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(SearchResult.class)
-                .block();
-
-        return response;
+        return naverSearchAPI.search();
     }
 }
 
