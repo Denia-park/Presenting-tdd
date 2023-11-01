1675d4293067577a406a63ea0c5a94c80eaefa84
HyunggiPark <phg9587@naver.com>
2023-11-01 13:33:31 +0900

    canCreateCustomer 추가 후 테스트 통과

---
+++ src/test/java/com/example/presenting_tdd/CustomerTest.java
@@ -0,0 +1,11 @@
+package com.example.presenting_tdd;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+    @Test
+    void canCreateCustomer() {
+        Customer customer = new Customer();
+
+    }
+}
