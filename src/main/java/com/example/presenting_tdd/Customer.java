716bcacb6e0dbac45b489a45a4be2207532e3575
HyunggiPark <phg9587@naver.com>
2023-11-01 13:57:09 +0900

    컴파일 에러 해결 후 테스트 통과하도록 값을 직접 반환

--- src/main/java/com/example/presenting_tdd/Customer.java
+++ src/main/java/com/example/presenting_tdd/Customer.java
@@ -1,4 +1,21 @@
 package com.example.presenting_tdd;
 
+enum MovieType {
+    CHILDRENS,
+    REGULAR,
+    NEW_RELEASES
+}
+
 public class Customer {
+
+    public void addRental(final String title, final MovieType movieType, final int days) {
+    }
+
+    public Integer getFrequenceRenterPoints() {
+        return 1;
+    }
+
+    public Double getCharge() {
+        return 1.5;
+    }
 }
--- src/test/java/com/example/presenting_tdd/CustomerTest.java
+++ src/test/java/com/example/presenting_tdd/CustomerTest.java
@@ -2,10 +2,26 @@ package com.example.presenting_tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CustomerTest {
     @Test
     void canCreateCustomer() {
         Customer customer = new Customer();
 
     }
+
+    @Test
+    void childrensMovieDaysRentedLTE3() {
+        // Arrange
+        final Customer c = new Customer();
+
+        // Act
+        c.addRental("childresMovie", MovieType.CHILDRENS, 3);
+
+        // Assert
+        assertThat(c.getFrequenceRenterPoints()).isEqualTo(1);
+        assertThat(c.getCharge()).isEqualTo(1.5);
+    }
+
 }
