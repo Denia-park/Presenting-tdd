29182f1b033578d6e80d59aa0a11640591c8bdf2
HyunggiPark <phg9587@naver.com>
2023-11-01 14:01:09 +0900

    Refactoring
    - remove duplication
    - remove unnecessary test(stairstep test)

--- src/test/java/com/example/presenting_tdd/CustomerTest.java
+++ src/test/java/com/example/presenting_tdd/CustomerTest.java
@@ -1,27 +1,29 @@
 package com.example.presenting_tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomerTest {
-    @Test
-    void canCreateCustomer() {
-        Customer customer = new Customer();
 
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer();
     }
 
     @Test
     void childrensMovieDaysRentedLTE3() {
         // Arrange
-        final Customer c = new Customer();
 
         // Act
-        c.addRental("childresMovie", MovieType.CHILDRENS, 3);
+        customer.addRental("childresMovie", MovieType.CHILDRENS, 3);
 
         // Assert
-        assertThat(c.getFrequenceRenterPoints()).isEqualTo(1);
-        assertThat(c.getCharge()).isEqualTo(1.5);
+        assertThat(customer.getFrequenceRenterPoints()).isEqualTo(1);
+        assertThat(customer.getCharge()).isEqualTo(1.5);
     }
 
 }
