7e8095f8ab7eb666e0211f0dd98e5f3d62fd9cdd
HyunggiPark <phg9587@naver.com>
2023-11-02 16:36:55 +0900

    사용되지 않는 findMovieBy 메서드 삭제

--- src/main/java/com/example/presenting_tdd/Rental.java
+++ src/main/java/com/example/presenting_tdd/Rental.java
@@ -12,10 +12,6 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    private Movie findMovieBy(final String title, final Movie.MovieType type) {
-        return MovieRepository.findMovieBy(title, type);
-    }
-
     public Integer getFrequenceRenterPoints(final int daysRented1) {
         return movie.getFrequenceRenterPoints(daysRented1);
     }
